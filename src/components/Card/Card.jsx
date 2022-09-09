53842a7d834634b014221eecb7720c78ecc2db26
Lucas Réquia <54502431+reqquia@users.noreply.github.com>
2022-09-09 12:53:39 -0300

    Update Estrutura
    Re-Design da versão desktop e alguns ajustes de design gerais.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./styles.css";
 
-export function Header() {
+export function Card() {
   return (
     <>
       <div className="container p-4 mb-0">
         <div className="row">
-          <div className="card shadow rounded p-4">
-            <div className="card-body">
-              <div className="col-12 p-2">
+          <div className="col-12 col-lg-5 mx-auto p-0">
+            <div className="card shadow rounded p-3">
+              <div className="card-body">
+                <div className="text-center mb-4">
+                  <img src="/logo.png" className="img-fluid" alt="logo" width={100}/>
+                </div>
+                
                 <div className="ratio ratio-1x1 shadow rounded-circle overflow-hidden">
                   <img
                     className="img-fluid img-cover"
@@ -17,15 +21,15 @@ export function Header() {
                   />
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="col-12 text-center">
-                <h1 className="display-5 mb-0">Lucas Réquia</h1>
-                <p className="fw-bold">Programador Fullstack</p>
-                <marquee>
-                  “Programadores e artistas são os únicos profissionais que tem
-                  como hobby a própria profissão.” – Rafael Lain
-                </marquee>
+              <div className="row">
+                <div className="col-12 text-center">
+                  <h1 className="display-5 mb-0">Lucas Réquia</h1>
+                  <p className="fw-bold titulo--color--primary">Programador Fullstack</p>
+                  <marquee>
+                    “Programadores e artistas são os únicos profissionais que
+                    tem como hobby a própria profissão.” – Rafael Lain
+                  </marquee>
+                </div>
               </div>
             </div>
           </div>
